683275dfbd88ff2b426b82222ed1e04b5e06614c
zionlew <zionlew@163.com>
2015-01-21 21:57:48 +0800

    First Commit

---
+++ src/gitdemo/Test.java
@@ -0,0 +1,24 @@
+/**----------------------------------------------------------+
+ +   PGM-ID  :   Test.java
+ +   Author  :   Liu Huan
+ +   Date    :   2015年1月21日 下午9:52:10
+ +   Function:   
+ +               
+ +-----------------------------------------------------------*/
+package gitdemo;
+
+/**
+ * @author LH
+ *
+ */
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

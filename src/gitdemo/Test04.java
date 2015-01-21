3afa18000ed051969ce11cd44dbb93b44d3687ad
zionlew <zionlew@163.com>
2015-01-21 23:24:24 +0800

    添加功能注释

--- src/gitdemo/Test04.java
+++ src/gitdemo/Test04.java
@@ -2,7 +2,7 @@
  +   PGM-ID  :   Test02.java
  +   Author  :   Liu Huan
  +   Date    :   2015年1月21日 下午11:14:58
- +   Function:   
+ +   Function:   多大的
  +               
  +-----------------------------------------------------------*/
 package gitdemo;
